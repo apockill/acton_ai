7d5cd2087c9de7b3cbcf4972e4750fad962d8348
Alex Thiele <alex@urbanmachine.build>
2024-11-05 15:49:33 -0800

    Update calibration script

--- acton_ai/scripts/calibrate_robot.py
+++ acton_ai/scripts/calibrate_robot.py
@@ -1,25 +1,24 @@
-import logging
+from time import sleep
 
 from acton_ai.connection_utilities import find_myarm_motor
+from acton_ai.logger import logger
 
 
 def main():
-    logging.basicConfig(level=logging.DEBUG)
-
     mover = find_myarm_motor()
+    mover.set_robot_power_on()
+
+    # Get the mover in a zero state
+    for i in range(7):
+        mover.set_joint_angle(i + 1, angle=0, speed=10)
+        sleep(0.5)
+
+    logger.error(f"Joint Positions: {mover.get_joints_angle()}")
 
     input(
-        "Enter the 'Calibration' Menu of your MyArmC and select 'Calibrate Servos'."
-        "Press Enter when ready."
+        f"Move joint {i + 1} to match the MyArmM. Then press 'Next' on the MyArmC'."
+        f" Press Enter when finished."
     )
-
-    mover.set_robot_power_on()
-    for i in range(6):
-        mover.set_joint_angle(i + 1, 0, 1)
-        input(
-            f"Move joint {i + 1} to match the MyArmM. Then press 'Next' on the MyArmC'."
-            f" Press Enter when finished."
-        )
 
 
 if __name__ == "__main__":
