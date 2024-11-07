999f972729bad42b7e7cc54b03a3b884b5d69844
Alex Thiele <alex@urbanmachine.build>
2024-11-07 08:57:32 -0800

    Add teleop and validate_robot scripts

--- acton_ai/scripts/validate_robot.py
+++ acton_ai/scripts/validate_robot.py
@@ -1,18 +1,27 @@
+from pymycobot import MyArmC, MyArmM
+
 from acton_ai.connection_utilities import find_myarm_controller, find_myarm_motor
 from acton_ai.logger import logger
 
 
 def main() -> None:
-    logger.info("Bringing up motors")
+    logger.info("Connecting to motors")
     controller = find_myarm_controller()
     mover = find_myarm_motor()
 
-    # Get the mover in a known state
-    mover.bring_up_motors()
+    logger.info("Controller information:")
+    print_robot_info(controller)
 
-    while True:
-        controller_angles = controller.get_joints_angle()
-        mover.set_joints_from_controller_angles(controller_angles, speed=50)
+    logger.info("Mover information:")
+    print_robot_info(mover)
+
+
+def print_robot_info(robot: MyArmM | MyArmC) -> None:
+    logger.info(f"Python Version: {robot.check_python_version()}")
+    logger.info(f"Robot Firmware Version: {robot.get_robot_firmware_version()}")
+    logger.info(f"Robot Modified Version: {robot.get_robot_modified_version()}")
+    logger.info(f"Tool Firmware Version: {robot.get_robot_tool_firmware_version()}")
+    logger.info(f"Tool Modified Version: {robot.get_robot_tool_modified_version()}")
 
 
 if __name__ == "__main__":
