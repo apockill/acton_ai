6f7f0b518f536db279e14ca1c78fae5a0436660a
Alex Thiele <alex@urbanmachine.build>
2024-11-15 10:56:44 -0800

    Add useful imports to the __init__

--- acton_ai/__init__.py
+++ acton_ai/__init__.py
@@ -4,3 +4,8 @@ My reppolicies for robot arms, and making them useful with multi-modal LLMs.
 """
 
 __version__ = "0.1.0"
+
+from pymycobot import MyArmC
+
+from .connection_utilities import find_myarm_controller, find_myarm_motor
+from .mover_wrapper import HelpfulMyArmM
